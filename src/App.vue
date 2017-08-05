0b414c884b320251bc75bdffe2e3e100d36e01c3
4edorov <4edorov@gmail.com>
2017-08-05 10:30:12 +0300

    countdown method added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="app">
-    <CurrentTime/></CurrentTime/>
+  <div>
+    <div>
+      <CurrentTime></CurrentTime>
+    </div>
+    <div>
+      <b-badge>{{diffMin}}:{{this.diffSec}}</b-badge>
+    </div>
   </div>
 </template>
 
@@ -11,17 +16,32 @@ export default {
   name: 'app',
   components: {
     CurrentTime
+  },
+  data () {
+    return {
+      workTime: 1,
+      diffMin: '',
+      diffSec: '',
+      currentTime: Date.now(),
+      workInt: ''
+    }
+  },
+  mounted () {
+    this.workInt = setInterval(this.restWorkTime, 1000)
+  },
+  methods: {
+    restWorkTime () {
+      let completitionTime = this.currentTime + this.workTime * 60 * 1000
+      let diffTime = completitionTime - Date.now()
+      this.diffMin = Math.floor(diffTime / 1000 / 60)
+      this.diffSec = Math.floor(diffTime / 1000 % 60)
+      if (this.diffMin === 0 && this.diffSec === 0) {
+        clearInterval(this.workInt)
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/components/CurrentTime.vue
+++ src/components/CurrentTime.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="current-time">
-    
+    <b-badge>
+      {{time}}<br />
+      {{date}}
+    </b-badge>
   </div>
 </template>
 
@@ -9,12 +12,23 @@ export default {
   name: 'CurrentTime',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      time: '',
+      date: ''
+    }
+  },
+  mounted () {
+    setInterval(this.updateTimeAndDate, 1000)
+  },
+  methods: {
+    updateTimeAndDate () {
+      let valueDate = new Date()
+
+      this.time = valueDate.toLocaleTimeString()
+      this.date = valueDate.toDateString()
     }
   }
 }
 </script>
 
 <style scoped>
-
 </style>
